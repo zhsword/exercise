3e89c7048b05b2214074380ac78a010baece4d3b
zhsword <zhsworld@126.com>
2018-02-11 10:29:04 +0800

    Update Tower of Hanoi.py

--- Tower of Hanoi.py
+++ Tower of Hanoi.py
@@ -1,3 +1,11 @@
 # 利用递归函数移动汉诺塔
 def move(n, a, b, c):
-
+  if n == 1:
+   print (a, '-->', b)
+  else:
+    move(n-1, a, c, b) #将a的前n-1个盘子从a移动到b
+    print (a, '-->', c) #将a最下面的圆盘移动到c
+    move(n-1, b, a, c) #将b的n-1个盘子从b移动到c
+   
+n = int(input（'请输入汉诺塔的层数：'）)
+move (n, 'X', 'Y', 'Z')
