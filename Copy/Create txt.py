100ec52647120437147ff29aa5ed102a042c0693
zhsword <zhsworld@126.com>
2018-02-22 21:14:52 +0800

    Update Create txt.py

--- Copy/Create txt.py
+++ Copy/Create txt.py
@@ -5,7 +5,7 @@ def file_write(file_name):
 	while True:
 		write_content = input()
 		if write_content != ':c':
-			f.write('%s\n' % write_content)
+			f.write('%s\n' % write_content) #/n每输入一句话，自动换行。如果没有，则无法换行
 		else:
 			break			
 	f.close()
