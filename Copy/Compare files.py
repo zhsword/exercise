840d305294fb2b36ca61e08158b0143b60f69fa3
zhsword <zhsworld@126.com>
2018-02-25 17:59:23 +0800

    Update Compare files.py

--- Copy/Compare files.py
+++ Copy/Compare files.py
@@ -25,3 +25,17 @@ else:
 	print('两个文件有 %d 处不同' % len(differ))
 	for each in differ:
 	print('第 %d 行不一样' % each)
+
+#以下利用with语句，保证文件的关闭
+def file_compare(file1, file2):
+    with open(file1) as f1, open(file2) as f2:
+        count = 0 # 统计行数
+        differ = [] # 统计不一样的数量
+
+        for line1 in f1:
+            line2 = f2.readline()
+            count += 1
+            if line1 != line2:
+                differ.append(count)
+
+    return differ
